89d4afedb393e95974f27d8c076936a56ee3ec73
Derrick Schwabe <admin@nerdbusiness.com>
2016-11-04 14:59:47 -0600

    Follow standard callback(err, payload) pattern.

--- underpouch.js
+++ underpouch.js
@@ -8,10 +8,10 @@ _pouch = {}
 
 _pouch.find = function(db, truthTest, callback) {
   db.allDocs({include_docs: true}, function(err, res) {
-    if(err) return console.log(err)
+    if(err) return callback(err)
     docs = _.pluck(res.rows, 'doc')
     doc = _.find(docs, truthTest)
-    return callback(doc)
+    return callback(null, doc)
   })
 }
 
@@ -19,18 +19,18 @@ _pouch.where = function(db, properties, callback) {
   if(_.isFunction(db.createIndex)) { 
     db.find({
       selector: properties
-    }, function(err2, res2) {
-      if(err2) return console.log(err2)                 
-      return callback(res2.docs)
+    }, function(err, res) {
+      if(err) return callback(err)                
+      return callback(null, res.docs)
     })
   } else {
     db.allDocs({include_docs: true}, function(err, res) {
-      if(err) return console.log(err)
+      if(err) return callback(err) 
       docs = _.chain(res.rows)
               .pluck('doc')
               .where(properties)
               .value()
-      return callback(docs)
+      return callback(null, docs)
     })
   }  
 }
@@ -42,15 +42,15 @@ _pouch.findWhere = function(db, properties, callback) {
       selector: properties, 
       limit: 1
     }, function(err, res) {
-      if(err) return console.log(err)
-      return callback(res.docs[0])
+      if(err) return callback(err) 
+      return callback(null, res.docs[0])
     })
   } else { //Otherwise just do an in-memory one time query:  
     db.allDocs({include_docs: true}, function(err, res) {
-      if(err) return console.log(err)
+      if(err) return callback(err) 
       docs = _.pluck(res.rows, 'doc')
       doc = _.findWhere(docs, properties)
-      return callback(doc)
+      return callback(null, doc)
     })    
   }
 }
@@ -69,17 +69,17 @@ var extendDoc = function(db, destinationDoc, sourceDoc, callback) {
   destinationDoc._rev = destinationDocRev
   //so we can now save it back into the db: 
   db.put(destinationDoc, function(err, res) {
-    if(err) return console.log(err)
+    if(err) return callback(err) 
     //Now update the doc once more with the latest rev...
     destinationDoc._rev = res.rev
     //and return it so the end user has the latest doc/rev:
-    return callback(destinationDoc)
+    return callback(null, destinationDoc)
   })  
 }
 
 _pouch.extend = function(db, destinationDocId, sourceDoc, callback) {
   db.get(destinationDocId, function(err, destinationDoc) {
-    if(err) return console.log(err)
+    if(err) return callback(err) 
     extendDoc(db, destinationDoc, sourceDoc, callback)
   })
 }
@@ -89,9 +89,9 @@ _pouch.extendPut = function(db, destinationDocId, sourceDoc, callback) {
     //If the doc is missing, simply do a routine put(): 
     if(err && err.reason == 'missing') {
       db.put(sourceDoc, function(err, res) {
-        if(err) return console.log(err) 
+        if(err) return callback(err)  
         sourceDoc._rev = res.rev
-        return callback(sourceDoc)
+        return callback(null, sourceDoc)
       })
     } else { //Otherwise, extend it: 
       extendDoc(db, destinationDoc, sourceDoc, callback)      
@@ -102,7 +102,7 @@ _pouch.extendPut = function(db, destinationDocId, sourceDoc, callback) {
 
 _pouch.merge = function(db, destinationDocId, sourceDoc, callback) {
   db.get(destinationDocId, function(err, destinationDoc) {
-    if(err) return console.log(err)
+    if(err) return callback(err) 
     var destinationDocRev = destinationDoc._rev
     //Merge with the sourceDoc...
     destinationDoc = _merge(destinationDoc, sourceDoc)
@@ -110,12 +110,12 @@ _pouch.merge = function(db, destinationDocId, sourceDoc, callback) {
     destinationDoc._rev = destinationDocRev
     //so we can now save it back into the db: 
     db.put(destinationDoc, function(err, res) {
-      if(err) return console.log(err)
+      if(err) return callback(err) 
       //Now update the doc once more with the latest rev...
       destinationDoc._rev = res.rev
 
       //and return it so the end user has the latest doc/rev:
-      return callback(destinationDoc)
+      return callback(null, destinationDoc)
     })
   })  
 }
@@ -125,9 +125,9 @@ _pouch.merge = function(db, destinationDocId, sourceDoc, callback) {
 
 _pouch.all = function(db, callback) {
   db.allDocs({include_docs: true}, function(err, res) {
-    if(err) return console.log(err)
+    if(err) return callback(err) 
     docs = _.pluck(res.rows, 'doc')
-    return callback(docs)
+    return callback(null, docs)
   })
 }
 
@@ -137,7 +137,7 @@ _pouch.replace = function(db, doc, callback) {
       //doc does not exist, so just post it: 
       return db.post(doc, callback)
     } else if(err) {
-      return console.log(err)
+      return callback(err) 
     }
     //Otherwise, update the rev and then put: 
     doc._rev = existingDoc._rev
